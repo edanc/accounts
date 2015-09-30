45e0f4503888b7b3684f9cd5188d9dbc077f6ce4
Ernesto Cojulu <erniecojulun@gmail.com>
2015-09-29 21:13:20 -0700

    record index table

--- app/assets/javascripts/components/records.js.jsx
+++ app/assets/javascripts/components/records.js.jsx
@@ -1,4 +1,11 @@
 var Records = React.createClass({
+  getInitialState: function() {
+    return { records: this.props.data };
+  },
+
+  getDefaultProps: function() {
+    return { records: [] };
+  },
 
   render: function() {
     return(
@@ -6,6 +13,21 @@ var Records = React.createClass({
         <h2 className='title'>
           Records
         </h2>
+        <table className='table table-bordered'>
+          <thead>
+            <tr>
+              <th> Date </th>
+              <th> Title </th>
+              <th> Amount </th>
+            </tr>
+          </thead>
+          <tbody>
+          {this.state.records.map(function(record) {
+            return <Record key={record.id} record={record} />
+           })}
+          </tbody>
+        </table>
       </div>
-      );}
+    );}
 });
+
