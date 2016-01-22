0e803c3e959c1abebd2ceac4af9876c338cefc1a
Ernesto Cojulun <erniecojulun@gmail.com>
2016-01-21 18:53:47 -0800

    added css class to form, moved concerns folder to app. added services folder.

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,8 @@ module Accounts
     config.react.addons = true
       # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    config.autoload_paths += %W( #{config.root}/concerns
+    config.autoload_paths += %W( #{config.root}app/concerns
+                                 #{config.root}app/services
                                )
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
