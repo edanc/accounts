609240936a119549f33ec226c52df8e8ec820f41
Ernesto Cojulun <erniecojulun@gmail.com>
2016-04-23 10:51:35 -0700

    labor line items model and migration

---
+++ app/models/labor_line_item.rb
@@ -0,0 +1,16 @@
+# == Schema Information
+#
+# Table name: labor_line_items
+#
+#  id         :integer          not null, primary key
+#  created_at :datetime
+#  updated_at :datetime
+#  log_id     :integer          not null
+#  amount     :decimal(20, 2)   not null
+#  deleted_at :datetime
+#
+
+class LaborLineItem < ActiveRecord::Base
+  belongs_to :log, inverse_of: :labor_line_items
+  acts_as_paranoid
+end
---
+++ db/migrate/20160214211522_labor_line_items.rb
@@ -0,0 +1,10 @@
+class LaborLineItems < ActiveRecord::Migration
+  def change
+    create_table :labor_line_items do |t|
+      t.timestamps
+    end
+    add_column :labor_line_items, :log_id, :integer, null: false
+    add_column :labor_line_items, :amount, :decimal, precision: 20, scale: 2 , null: false
+    add_column :labor_line_items, :deleted_at, :datetime
+  end
+end
